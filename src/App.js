2f87a980be2f7728efb2610c1db675327d912350
Harshita-2000 <harshigarg2000@gmail.com>
2021-09-01 12:40:34 +0530

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState(null);
+    const fetchData = async () => {
+        const jsonData = await fetch("https://reqres.in/api/users?page=1");
+        const data = await jsonData.json();
+        console.log(data);
+        setData(data.data);
+    }
+    return (
+        <>
+            <div className="navbar">
+                <h1 className="navbar-heading">Harshita Networks</h1>
+            </div>
+            <div className="card-container">
+                {
+                    data?(data.map((ele) => {
+                        return (
+                            <div className="card" key={ele.id}>
+                                <div className="card-top">
+                                    <img src={ele.avatar} alt="sarwar" className="card-img" />
+                                </div>
+                                <div className="card-bottom">
+                                    <h4>{`${ele.first_name} ${ele.last_name}`}</h4>
+                                    <p>{ele.email}</p>
+                                </div>
+                            </div>
+                        )
+                    }
+                    )):(<button className="navbar-btn" onClick={fetchData}>Get Users</button>)
+                }
+            </div>
+        </>
+    )
 }
 
 export default App;
